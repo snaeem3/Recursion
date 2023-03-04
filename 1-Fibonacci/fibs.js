868c5f4c9e3a6cb43053f7894ddb0a2b55eff7cf
Sameer <sameer4363@gmail.com>
2023-03-04 18:20:31 -0500

    Add working fibs.js

---
+++ 1-Fibonacci/fibs.js
@@ -0,0 +1,21 @@
+function fibs(num) {
+    if (num === 0) {
+        return [];
+    }
+
+    if (num === 1) {
+        return [0];
+    }
+
+    let result = [0, 1];
+
+    for (i = 2; i < num; i++) {
+        result.push(result[i-2] + result[i-1]);
+    }
+
+    return result;
+}
+
+console.log(fibs(1));
+console.log(fibs(2));
+console.log(fibs(8));
